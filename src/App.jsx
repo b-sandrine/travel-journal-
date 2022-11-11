69408aee4069655ee4019196733e9a14d651a478
b-sandrine <sanrdushimimana@gmail.com>
2022-11-11 13:51:24 +0200

    Add props & Mapping. Final Card Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Navbar from "./components/Navbar"
 import Card from "./components/Card"
-
+import data from "../public/data"
 export default function App() {
+
+  const placeDetails = data.map((place) => {
+    return <Card id = {place.id} {...place} />
+  })
   return(
     <section>
       <Navbar />
-      <Card />
+      <div className="item">
+          {placeDetails}
+      </div>
     </section>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,21 @@
 
 import {FaMapMarkerAlt} from 'react-icons/fa'
-function Card() {
+function Card(props) {
     return (
         <div className="container">
             <div className="card--image">
-                <img src={`https://source.unsplash.com/WLxQvbMyfas`} />
+                <img src={`${props.image}`} />
             </div>
             
             <div className="description">
                 <div className="card--place">
                     <FaMapMarkerAlt className='card--icon'/>
-                    <p className="card--title">JAPAN</p>
-                    <a className="card--link">View on Google Map</a>
+                    <p className="card--title">{props.location}</p>
+                    <a className="card--link" href={props.googleLink}>View on Google Map</a>
                 </div>
-                <h1 className='card--heading'>Mount Fuji</h1>
-                <h4 className='card--timeline'>12 Jan, 2021 - 12 Jan, 2021</h4>
-                <p className='card--description'>
-                "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet).
-                Mount Fuji is the single most popular tourist site 
-                in Japan, for both Japanese and foreign tourists.
-                </p>
+                <h1 className='card--heading'>{props.title}</h1>
+                <h4 className='card--timeline'>{props.startDate} - {props.endDate}</h4>
+                <p className='card--description'>{props.description}</p>
             </div>
         </div>
     )
